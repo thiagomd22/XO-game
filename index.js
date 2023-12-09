df46c4c47405647bfb35fa057ae600818ee7307c
Thiago Meireles <thiago22md@gmail.com>
2023-12-09 16:59:36 -0300

    function_gameOver

--- index.js
+++ index.js
@@ -58,3 +58,12 @@ function resetGame() {
     playerTurn = "x";
 }
 
+function gameOver(a) {
+    var gameOverAlertElement = "<b>GAME OVER </b><br><br> Player " + span[a].dataset.player.toUpperCase() + ' Win !!! <br><br>' + restartButton;
+    var div = document.createElement("div");
+    div.className = "alert";
+    div.innerHTML = gameOverAlertElement;
+    document.getElementsByTagName("body")[0].appendChild(div);
+    window.isGameOver = true;
+    moves = 0;
+}
